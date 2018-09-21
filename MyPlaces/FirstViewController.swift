f93111f7845a0bd515b3ee11631b97e1d7d693bd
user145617 <user145617@87-237-62-10.rdns.melbourne.co.uk>
2018-09-21 13:09:42 -0700

    Initial Commit

---
+++ MyPlaces/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyPlaces
+//
+//  Created by user145617 on 9/21/18.
+//  Copyright © 2018 UOC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
