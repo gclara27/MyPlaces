8d236828dd1974d70c2ea56caff3ccc347126512
user145617 <user145617@eu610.local>
2018-10-08 21:18:12 +0100

    Final version

---
+++ MyPlaces/PlaceTourist.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceTourist.swift
+//  MyPlaces
+//
+//  Created by user145617 on 9/25/18.
+//  Copyright © 2018 UOC. All rights reserved.
+//
+
+import Foundation
+
+class PlaceTourist: Place {
+    var DiscountTourist:String = ""
+    
+    override init() {
+        super.init()
+        PlaceType = .TouristicPlace
+    }
+    
+    init(name:String, description:String, discountTourist:String, image_in:Data?) {
+        super.init(type:.TouristicPlace, name:name, description:description, image_in:image_in)
+        self.DiscountTourist = discountTourist
+    }
+}
